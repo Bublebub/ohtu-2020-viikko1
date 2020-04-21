f027c9fdc813dabc5157fe9d0aafe71ec00aa688
Elmeri Lehikoinen <elmeri.lehikoinen@gmail.com>
2020-04-21 13:41:19 +0300

    viikko2_tehtävä2

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Varasto mehua = new Varasto(100.0);
+        
+    }
+	
+	/*
+	Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
         System.out.println("Luonnin jälkeen:");
@@ -56,5 +60,5 @@ public class Main {
         saatiin = mehua.otaVarastosta(-32.9);
         System.out.println("saatiin " + saatiin);
         System.out.println("Mehuvarasto: " + mehua);
-    }
+		*/
 }
